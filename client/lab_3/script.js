581c3d18b90d597ca01d6fc288eabea63e12d508
Caleb Andree <candree@terpmail.umd.edu>
2023-02-13 18:43:39 -0500

    Adding code for moveToPrevSlide function

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -28,12 +28,12 @@ function updateSlidePosition() {
 }
 
 function moveToNextSlide() {
-  /*
-    add an if statement here that checks
-    if you're already at the max number of slides
-    and if so, sets your slidePosition to the first index of an array
-    if not, set the slidePosition to the current position plus one
-  */
+  if(slidePosition===totalSlides - 1){
+    slidePosition = 0;
+  }else{
+    slidePosition += 1;
+  }
+
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
@@ -44,6 +44,11 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+ if(slidePosition===0){
+  slidePosition = totalSlides - 1;
+ }else{
+  slidePosition-=1;
+ }
   updateSlidePosition();
 }
 
