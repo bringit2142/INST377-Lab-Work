5ecbebf196cd243062c9f798ba135c0b9905b94e
bringit2142 <102170516+bringit2142@users.noreply.github.com>
2023-02-27 23:30:33 -0500

    Lab 5 edits, incorporating JS forms

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -5,6 +5,12 @@
 
 /* A quick filter that will return something based on a matching input */
 function filterList(list, query) {
+  return list.filter((item)=> {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+
+  })
   /*
     Using the .filter array method, 
     return a list that is filtered by comparing the item name in lower case
@@ -16,13 +22,14 @@ function filterList(list, query) {
 
 async function mainEvent() { // the async keyword means we can make API requests
   const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const filterButton = document.querySelector('.filter_button');
   // Add a querySelector that targets your filter button here
 
   let currentList = []; // this is "scoped" to the main event function
   
   /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
   mainForm.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
-    
+  
     // This prevents your page from becoming a list of 1000 records from the county, even if your form still has an action set on it
     submitEvent.preventDefault(); 
     
@@ -53,6 +60,16 @@ async function mainEvent() { // the async keyword means we can make API requests
     */
     console.table(currentList); 
   });
+  filterButton.addEventListener('click', (event) => {
+    console.log('Clicked filterButton');
+
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+
+    console.log(newList)
+  })
 
 
   /*
