fb5608a313f7e9312d2021063bd454843a1c9073
bringit2142 <102170516+bringit2142@users.noreply.github.com>
2023-03-03 16:49:33 -0500

    Added function to get random resto

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -10,9 +10,24 @@
     Under this comment place any utility functions you need - like an inclusive random number selector
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 */
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
 
 function injectHTML(list) {
   console.log('fired injectHTML');
+  const target = document.querySelector('#restaurant_list');
+  target.innerHTML = '';
+
+  const listEl = document.createElement('ol');
+  target.appendChild(listEl);
+  list.forEach((item => {
+    const el = document.createElement('li');
+    el.innerText = item.name;
+    listEl.appendChild(el);
+  })
   /*
   ## JS and HTML Injection
     There are a bunch of methods to inject text or HTML into a document using JS
@@ -31,6 +46,12 @@ function injectHTML(list) {
 
 function processRestaurants(list) {
   console.log('fired restaurants list');
+  const range = [...Array(15).keys()];
+  const newArray = range.map((item) => {
+    const getIndex = getRandomIntInclusive(0, list.length);
+    return list[index];
+  })
+  return newArray;
 
   /*
     ## Process Data Separately From Injecting It
@@ -102,6 +123,7 @@ async function mainEvent() {
 
       // This constant will contain the value of your 15-restaurant collection when it processes
       const restaurantList = processRestaurants(arrayFromJson);
+      console.log(restaurantList);
 
       // And this function call will perform the "side effect" of injecting the HTML list for you
       injectHTML(restaurantList);
