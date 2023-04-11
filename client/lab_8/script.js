d69d624e219c0e23705a158bf9ba5543e26647a6
bringit2142 <102170516+bringit2142@users.noreply.github.com>
2023-04-10 22:20:12 -0400

    Map works, along with clear button

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -38,25 +38,53 @@ function filterList(list, query) {
   });
 }
 
+function initMap(){
+    const carto = L.map('map').setView([38.98, -76.93], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(carto);
+return carto;
+}
+
+function markerPlace(array,map){
+    console.log('array for markers', array);
+    map.eachLayer((layer) => {
+  if (layer instanceof L.Marker) {
+    layer.remove();
+  }
+});
+
+    array.forEach((item) =>{
+        console.log('markerPlace', item);
+        const {coordinates} = item.geocoded_column_1;
+
+        L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    })
+}
+
 
 
 async function mainEvent() {
   // the async keyword means we can make API requests
   const mainForm = document.querySelector(".main_form"); // This class name needs to be set on your form before you can listen for an event on it
   const loadDataButton = document.querySelector("#data_load");
+  const clearDataButton = document.querySelector("#data_clear");
   const generateListButton = document.querySelector("#generate");
   const textField = document.querySelector("#resto");
 
   const loadAnimation = document.querySelector("#data_load_animation");
   loadAnimation.style.display = "none";
   generateListButton.classList.add("hidden");
+
+  const carto = initMap();
   
 
   const storedData = localStorage.getItem('storedData');
-  const parsedData = JSON.parse(storedData);
-  if (parsedData.length > 0){
-    generateListButton.classList.remove("hidden");
-  }
+  let parsedData = JSON.parse(storedData);
+  if (parsedData?.length > 0){
+      generateListButton.classList.remove("hidden");
+  }   
 
   let currentList = []; // this is "scoped" to the main event function
   
@@ -77,7 +105,12 @@ async function mainEvent() {
     // This changes the response from the GET into data we can use - an "object"
     const storedList = await results.json();
     localStorage.setItem('storedData',JSON.stringify(storedList));
-   
+    parsedData = storedList;
+
+    if (parsedData?.length > 0){
+        generateListButton.classList.remove("hidden");
+  }   
+
     loadAnimation.style.display = "none";
 
     //console.table(storedList);
@@ -89,6 +122,7 @@ async function mainEvent() {
     currentList = cutRestaurantList(parsedData);
     console.log(currentList);
     injectHTML(currentList);
+    markerPlace(currentList, carto);
   });
 
   textField.addEventListener("input", (event) => {
@@ -96,8 +130,19 @@ async function mainEvent() {
     const newList = filterList(currentList, event.target.value);
     console.log(newList);
     injectHTML(newList);
+    markerPlace(newList, carto);
+
   });
+
+
+  clearDataButton.addEventListener("click", (event) => {
+    console.log('clear browser data');
+    localStorage.clear();
+    console.log('localStorage Check', localStorage.getItem("storedData"));
+})
 }
+
+
 
 /*
   This adds an event listener that fires our main event only once our page elements have loaded
